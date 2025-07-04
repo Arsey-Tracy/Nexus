17d834bec5b90d01c65063af2327f975b179dd4a
Mugaba Ashiraf <hanmas669@gmail.com>
2025-07-04 18:16:30 +0300

    Updated nexus project
    modified:   package-lock.json
    modified:   package.json
    new file:   src/components/ui/accordion.tsx
    new file:   src/components/ui/alert-dialog.tsx
    new file:   src/components/ui/alert.tsx
    new file:   src/components/ui/aspect-ratio.tsx
    new file:   src/components/ui/avatar.tsx
    new file:   src/components/ui/badge.tsx
    new file:   src/components/ui/breadcrumb.tsx
    new file:   src/components/ui/calendar.tsx
    new file:   src/components/ui/carousel.tsx
    new file:   src/components/ui/chart.tsx
    new file:   src/components/ui/checkbox.tsx
    new file:   src/components/ui/collapsible.tsx
    new file:   src/components/ui/command.tsx
    new file:   src/components/ui/context-menu.tsx
    new file:   src/components/ui/dialog.tsx
    new file:   src/components/ui/dropdown-menu.tsx
    modified:   src/components/ui/form.tsx
    new file:   src/components/ui/hover-card.tsx
    new file:   src/components/ui/navigation-menu.tsx
    new file:   src/components/ui/pagination.tsx
    modified:   src/components/ui/radio-group.tsx
    new file:   src/components/ui/resizable.tsx
    new file:   src/components/ui/scroll-area.tsx
    new file:   src/components/ui/select.tsx
    new file:   src/components/ui/separator.tsx
    new file:   src/components/ui/sheet.tsx
    new file:   src/components/ui/sidebar.tsx
    new file:   src/components/ui/skeleton.tsx
    new file:   src/components/ui/slider.tsx
    new file:   src/components/ui/sonner.tsx
    new file:   src/components/ui/switch.tsx
    new file:   src/components/ui/table.tsx
    new file:   src/components/ui/tabs.tsx
    new file:   src/components/ui/toast.tsx
    new file:   src/components/ui/toaster.tsx
    new file:   src/components/ui/toggle-group.tsx
    new file:   src/components/ui/toggle.tsx
    new file:   src/components/ui/tooltip.tsx
    new file:   src/components/ui/use-toast.ts

---
+++ src/components/ui/aspect-ratio.tsx
@@ -0,0 +1,10 @@
+/** @format */
+
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+
+const AspectRatio = AspectRatioPrimitive.Root;
+
+export { AspectRatio };
+pectRatioPrimitive.Root;
+
+export { AspectRatio };
